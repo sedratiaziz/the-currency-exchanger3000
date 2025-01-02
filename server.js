42aa6e94a92ff2cdef9bfda5a0630c17ff1e9480
sedratiaziz <87585184+sedratiaziz@users.noreply.github.com>
2025-01-02 17:43:01 +0300

    v1.1.0 fixed some problems

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-const axios = require("axios")
+const baseCurrencyEl = document.querySelector('#base-currency')
+const targetCurrencyEl = document.querySelector('#target-currency')
+
+// test whether it got the element
+console.log(baseCurrencyEl)
+
+// async function convert(firstCurr, secondCurr, amount) {
+//     try {
+//         const result = await axios.get(`https://v6.exchangerate-api.com/v6/90f90b5e3f18631c41ba8166/pair/${firstCurr}/${secondCurr}/${amount}`)
+//         console.log(`${result.data.conversion_result} ${result.data.target_code}`)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
 
-async function convert(firstCurr, secondCurr, amount) {
+// convert("kwd", "lbp", 1)
+
+
+async function test() {
     try {
-        const result = await axios.get(`https://v6.exchangerate-api.com/v6/90f90b5e3f18631c41ba8166/pair/${firstCurr}/${secondCurr}/${amount}`)
-        console.log(`${result.data.conversion_result} ${result.data.target_code}`)
+        const result = await axios.get(`https://v6.exchangerate-api.com/v6/90f90b5e3f18631c41ba8166/latest/usd`)
+        const currObj = result.data.conversion_rates;
+
+        for (const key in currObj) {
+            console.log(`${key}`)
+        }
+
     } catch (error) {
         console.log(error)
     }
 }
 
-
-convert("kwd", "lbp", 1)
-
-
+test()
