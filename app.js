8cc17a668f30f48ff10c92dac9a46196509cf5ee
sedratiaziz <87585184+sedratiaziz@users.noreply.github.com>
2025-01-06 14:38:20 +0300

    v2.1.0 Major internal logic fix

---
+++ app.js
@@ -0,0 +1,68 @@
+
+
+const baseSelectEl = document.querySelector("#baseSelect")
+const targetSelectEl = document.querySelector("#targetSelect")
+const convertBtn = document.querySelector("#convert-btn")
+const amountInput = document.querySelector("input")
+
+
+baseSelectEl.innerHTML = "<option>Loading...</option>"
+targetSelectEl.innerHTML = "<option>Loading...</option>"
+
+
+
+async function populate() {
+    try {
+        const result = await axios.get(`https://v6.exchangerate-api.com/v6/90f90b5e3f18631c41ba8166/latest/usd`)
+        const currObj = result.data.conversion_rates;
+        
+        baseSelectEl.innerHTML = ''
+        targetSelectEl.innerHTML = ''
+
+        for (const key in currObj) {
+            // console.log(`${key}`)
+            const optionEl = document.createElement('option')
+            optionEl.textContent = key;
+            optionEl.value = key
+            baseSelectEl.append(optionEl)
+            
+            
+            const optionSecondEl = document.createElement('option')
+            optionSecondEl.textContent = key;
+            optionSecondEl.value = key
+            targetSelectEl.append(optionSecondEl)
+          
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+populate();
+
+
+async function convert(baseValue, targetValue, amount) {
+    try {
+
+        const conversion_rate_obj = await axios.get(`https://v6.exchangerate-api.com/v6/90f90b5e3f18631c41ba8166/pair/${baseValue}/${targetValue}`)
+        const conversion_rate = conversion_rate_obj.data.conversion_rate;
+        const result = (amount * conversion_rate).toFixed(3);
+        const base_code = conversion_rate_obj.data.base_code;
+        const target_code = conversion_rate_obj.data.target_code;
+
+        alert(`${amount} ${base_code} is equal to ${result} ${target_code}`)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+convertBtn.addEventListener("click", async () => {
+    const baseValue = baseSelectEl.value;
+    const targetValue = targetSelectEl.value;
+    const amount = amountInput.value;
+
+    await convert(baseValue, targetValue, amount);
+})
--- server.js
+++ server.js
@@ -1,34 +1,16 @@
-const baseCurrencyEl = document.querySelector('#base-currency')
-const targetCurrencyEl = document.querySelector('#target-currency')
+const express = require("express")
+const app = express();
 
-// test whether it got the element
-console.log(baseCurrencyEl)
-
-// async function convert(firstCurr, secondCurr, amount) {
-//     try {
-//         const result = await axios.get(`https://v6.exchangerate-api.com/v6/90f90b5e3f18631c41ba8166/pair/${firstCurr}/${secondCurr}/${amount}`)
-//         console.log(`${result.data.conversion_result} ${result.data.target_code}`)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+app.use(express.static(__dirname))
 
 
-// convert("kwd", "lbp", 1)
+app.get("/", (req, res)=>{
+    res.send(__dirname + "/index.html")    
+})
 
 
-async function test() {
-    try {
-        const result = await axios.get(`https://v6.exchangerate-api.com/v6/90f90b5e3f18631c41ba8166/latest/usd`)
-        const currObj = result.data.conversion_rates;
 
-        for (const key in currObj) {
-            console.log(`${key}`)
-        }
 
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-test()
+app.listen(3000, ()=>{
+    console.log('listening on port 3000')
+})
